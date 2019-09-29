b9789c87a1d52990fe29ad98566fdbba7afc8663
马转香 <55611014+mazhuanxiang@users.noreply.github.com>
2019-09-29 15:18:16 +0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,24 @@
-#!/bin/bash
+1.#!/bin/bash
 echo "Hello World"
 echo "hello Beijing"
 echo "ni hao "
 echo "haha"
+2.#!/bin/bash 
+ss -l | grep httpd &> /dev/null
+if [ $? -eq 0 ] ;then
+  echo "httpd软件已安装"
+  exit
+else
+  yum -y install httpd 
+  systemctl start httpd
+  systemctl enable httpd
+fi 
+3. #!/bin/bash
+for i in  {1..10}
+ do 
+ ping -n 4 -c 0.2 -t 1 192.168.4.$i &> /dev/null  
+  if [ $? -eq 0 ] && echo "192.168.4.$i is up "
+  else 
+  echo echo "192.168.4.$i is down"
+  fi
+ done 
